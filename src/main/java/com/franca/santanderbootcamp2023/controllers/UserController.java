20691f58beb78f5e5865853763801f6459a51a98
RossyFranca <rossyfranca@gmail.com>
2023-10-05 13:32:47 -0300

    handle exception created

--- src/main/java/com/franca/santanderbootcamp2023/controllers/UserController.java
+++ src/main/java/com/franca/santanderbootcamp2023/controllers/UserController.java
@@ -3,11 +3,12 @@ package com.franca.santanderbootcamp2023.controllers;
 import com.franca.santanderbootcamp2023.domains.model.User;
 import com.franca.santanderbootcamp2023.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(path = "/users")
@@ -27,5 +28,12 @@ public class UserController {
     public ResponseEntity<User> findById(@PathVariable Long id){
         var user = userService.findById(id);
         return ResponseEntity.ok(user);
+    }
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+            var userCreated =  userService.create(user);
+            URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                    .path("/{id}").buildAndExpand(userCreated.getId()).toUri();
+            return ResponseEntity.created(location).body(userCreated);
     }
 }
