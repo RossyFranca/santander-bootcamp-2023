f365379bb70589dd1f3f9bcbf72024ae5ad17b80
RossyFranca <rossyfranca@gmail.com>
2023-10-05 09:08:49 -0300

    ajuste na modelagem

--- src/main/java/com/franca/santanderbootcamp2023/model/BaseItem.java
+++ src/main/java/com/franca/santanderbootcamp2023/model/BaseItem.java
@@ -1,5 +1,8 @@
 package com.franca.santanderbootcamp2023.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +11,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @MappedSuperclass
 public abstract class BaseItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String icon;
     private String description;
 }
--- src/main/java/com/franca/santanderbootcamp2023/model/News.java
+++ src/main/java/com/franca/santanderbootcamp2023/model/News.java
@@ -1,11 +1,8 @@
 package com.franca.santanderbootcamp2023.model;
 
 import jakarta.persistence.Entity;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
+
 @Entity(name = "tb_news")
 public class News extends BaseItem{
 
