9ee40704586bd90c64e06d68e2ecfe652095971e
RossyFranca <rossyfranca@gmail.com>
2023-10-05 14:21:33 -0300

    problema de CORS corrigido

--- src/main/java/com/franca/santanderbootcamp2023/Application.java
+++ src/main/java/com/franca/santanderbootcamp2023/Application.java
@@ -1,15 +1,19 @@
 package com.franca.santanderbootcamp2023;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class Application {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
 
-	}
+    }
 
 }
